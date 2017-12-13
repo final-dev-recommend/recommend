8fcb9ca30c57ac0de6e616e98168709f2dab8539
Erde-ap <mtdaier@gmail.com>
2017-12-13 14:16:49 +0900

    最低限必要なファイルを作成 トップページをコメントアウトしました。

---
+++ src/app/recommend/recommend.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recommend-top',
+  templateUrl: './recommend.component.html',
+  styles: [``]
+})
+export class AppComponent {
+  constructor() {
+  }
+}
